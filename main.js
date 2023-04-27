4c110194758e6a71af18d6653f74d7926eb7a5da
vickneee <victoria.vavulina@outlook.com>
2023-04-27 15:01:11 +0300

    Update

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -29,14 +33,25 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    
+    resetBoard();
 } 
         
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
         }, 1500);
             
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+} 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
